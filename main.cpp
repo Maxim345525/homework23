96289f7e546fe7a3b467d80c6be1442a6bd9c1f2
Maxim345525 <maximmaksimchyk@gmail.com>
2023-02-26 21:10:44 +0200

    add code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
-f
+#include<iostream>
+using namespace std;
+    int* removeNegativeNumbers(int* array, int size)
+    {
+        int nonNegCount = 0;
+        for (int i = 0; i < size; i++) {
+            if (array[i] >= 0) {
+                nonNegCount++;
+            }
+        }
+        int* newArray = new int[nonNegCount];
+        int j = 0;
+        for (int i = 0; i < size; i++) {
+            if (array[i] >= 0) {
+                newArray[j] = array[i];
+                j++;
+            }
+        }
+        delete[] array;
+        return newArray;
+    }
