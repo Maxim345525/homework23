3c320760f3cb4755c969bfefa917bc38f89fbc68
Maxim345525 <maximmaksimchyk@gmail.com>
2023-02-26 22:01:48 +0200

    add code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
-f
+#include<iostream>
+using namespace std;
+void insertElements(int*& array, int& size, int* elements, int numElements, int index) {
+    if (index < 0 || index > size) {
+        return;
+    }
+    int newSize = size + numElements;
+    int* newArray = new int[newSize];
+    for (int i = 0; i < index; i++) {
+        newArray[i] = array[i];
+    }
+    for (int i = 0; i < numElements; i++) {
+        newArray[index + i] = elements[i];
+    }
+    for (int i = index; i < size; i++) {
+        newArray[numElements + i] = array[i];
+    }
+    delete[] array;
+    array = newArray;
+    size = newSize;
+}
