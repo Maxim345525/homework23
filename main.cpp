ea647bb6c6b3f2be1e669dab84593b8c17e7d7fe
Maxim345525 <maximmaksimchyk@gmail.com>
2023-02-26 21:17:42 +0200

    add code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
-f
+#include<iostream>
+using namespace std;
+void add_to_array(int*& array, int& size, int* elements, int numElements) {
+    int newSize = size + numElements;
+    int* newArray = new int[newSize];
+    for (int i = 0; i < size; i++) {
+        newArray[i] = array[i];
+    }
+    for (int i = 0; i < numElements; i++) {
+        newArray[size + i] = elements[i];
+    }
+    delete[] array;
+    array = newArray;
+    size = newSize;
+}
